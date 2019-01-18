2007de35e4e56bf1b3eb2b9af863c85807b31347
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2019-01-17 23:51:38 -0500

    front end2

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -25,8 +25,14 @@ const createStar = async () => {
   App.setStatus("New Star Owner is " + account + ".");
 }
 
-// Add a function lookUp to Lookup a star by ID using tokenIdToStarInfo()
 
+// Add a function lookUp to Lookup a star by ID using tokenIdToStarInfo()
+const lookUptokenIdToStarInfo = async () => {
+  const instance = await StarNotary.deployed();
+  const token = document.getElementById('tokenId');
+  let name = await instance.lookUptokenIdToStarInfo(token);
+  starSpan.innerHTML = name;
+}
 //
 
 const App = {
@@ -55,13 +61,18 @@ const App = {
   },
 
   setStatus: function (message) {
-    const status = document.getElementById('status')
+    const status = document.getElementById('status');
     status.innerHTML = message
   },
 
   createStar: function () {
     createStar();
   },
+lookUptokenIdToStarInfo : function(){
+  lookUptokenIdToStarInfo();
+  
+},
+
 
 }
 
@@ -71,24 +82,24 @@ window.addEventListener('load', function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     console.warn(
-      `Using web3 detected from external source.
-       If you find that your accounts don\'t appear or you have 0 MetaCoin,
-       ensure you\'ve configured that source properly.
-       If using MetaMask, see the following link.
-       Feel free to delete this warning. :)
-       http://truffleframework.com/tutorials/truffle-and-metamask`
+      'Using web3 detected from external source.' +
+      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+      ' ensure you\'ve configured that source properly.' +
+      ' If using MetaMask, see the following link.' +
+      ' Feel free to delete this warning. :)' +
+      ' http://truffleframework.com/tutorials/truffle-and-metamask'
     )
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider)
   } else {
     console.warn(
-      `No web3 detected. Falling back to http://127.0.0.1:9545.
-       You should remove this fallback when you deploy live, as it\'s inherently insecure.
-       Consider switching to Metamask for development.
-       More info here: http://truffleframework.com/tutorials/truffle-and-metamask`
+      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
+      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+      ' Consider switching to Metamask for development.' +
+      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
     )
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
   }
 
   App.start()
