b94ca73c0ff9f7b9a6935274a7805e94eba2c062
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2019-01-24 17:30:10 -0500

    test approve

--- test/starNotary.js
+++ test/starNotary.js
@@ -38,8 +38,8 @@ assert.equal( await instance.getSymbolName(), symbol);
 it('2 users can exchange their stars',async ()=>{
 let starId1 = 1;
 let starId2 = 2;
-let user1 = accounts[3];
-let user2 = accounts[4];
+let address1 = accounts[3];
+let address2 = accounts[4];
 let na = 'Pluton';
 let na1 = 'Neptuno';
 let  ra = "16h 29m 1.0s";
@@ -53,11 +53,11 @@ let cent1 = 65;
 let story = "Found star using https://www.google.com/sky/";
 let story1 = "Found star using https://www.google.com/neptuno/";
 await instance.createStar(na, ra, dec,  mag,  cent,  story,starId1 ,{from: user1});
-await instance.createStar(na1, ra1, dec1,  mag1,  cent1,  story1, starId2,{from: user2});
-await instance.approve(starId1);
-await instance.exchangeStars.call(starId1, starId2);
-assert.equal(await instance.ownerOf.call(tokenId1),user2 );
-assert.equal(await instance.ownerOf.call(tokenId2),user1 );
+await instance.createStar(na1, ra1, dec1,  mag1,  cent1,  story1, starId2,{from: address2});
+await instance.approve(address1, starId2,{from: address2});
+await instance.exchangeStars.call(starId1, starId2, {from:address1});
+assert.equal(await instance.ownerOf.call(tokenId1),address2 );
+assert.equal(await instance.ownerOf.call(tokenId2),address1 );
 });
 
 //Code execution fails at some require() or (caller must be owner or authorized users), insufficient eth sent.
@@ -65,7 +65,7 @@ assert.equal(await instance.ownerOf.call(tokenId2),user1 );
 it('Stars Tokens can be transferred from one address to another.',async ()=>{
 let receiver = accounts[6];
 let token = 6;
-// await instance.approve(token);
+await instance.approve(token);
 await instance.transferStar(receiver,token, {from:receiver});
 assert.equal(await instance.ownerOf.call(token), receiver)
 });
