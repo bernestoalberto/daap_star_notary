86f52aa97c836206a9f2b1fcc5c69f790257178d
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2019-01-29 16:28:08 -0500

    token distintc

--- test/starNotary.js
+++ test/starNotary.js
@@ -15,25 +15,16 @@ owner = accounts[0];
 instance = await StarNotary.deployed();
 });
 
-it('can Create a Star', async() => {
-
-
-
-});
-
-
-  
-
-
-
-    //1. Create a star
-    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+   
+    
 
 it('can add the star name and star symbol properly', async() => {
 let name = 'ErnestoBM Token';
 let symbol = 'EBM';
 let tokenId = 1;
+ //1. Create a star
 await instance.createStar("Pluton",tokenId,{from: owner});
+//2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
 assert.equal( await instance.NAME.call(), name);
 assert.equal( await instance.SYMBOL.call(), symbol);
 });
@@ -42,8 +33,8 @@ assert.equal( await instance.SYMBOL.call(), symbol);
     
 it('2 users can exchange their stars',async ()=>{
 
-let starId1 = 1;
-let starId2 = 2;
+let starId1 = 2;
+let starId2 = 3;
 let address1 = accounts[3];
 let address2 = accounts[4];
 
@@ -66,7 +57,7 @@ assert.equal(await instance.ownerOf(starId1),address2 );
 
 it('Stars Tokens can be transferred from one address to another.',async ()=>{
     let receiver = accounts[7];
-    let token = 1;
+    let token = 4;
      // 1. create a Star
     await instance.createStar("Pluton",token,{from: owner});
     await instance.approve.call(receiver,token,{from:owner});
